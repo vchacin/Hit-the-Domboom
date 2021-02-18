5c62756767791d4f976cf644048c315676bf73dc
vchacin <arq.vchacin@gmail.com>
2021-02-18 18:16:02 -0300

    scripts por id para cambiar por

--- scripts.js
+++ scripts.js
@@ -1,21 +1,25 @@
 
 let bomboaudio = document.getElementById ("bombo1");
     function playaudio1() {
-         bomboaudio.play();
+        bomboaudio.currentTime= 0;
+        bomboaudio.play();
      }
 
 let hithataudio = document.getElementById ("hithat");
     function playaudio2() {
+        hithataudio.currentTime= 0;
         hithataudio.play();
      }
         
 let redoblanteaudio = document.getElementById ("redoblante");
     function playaudio3() {
+        redoblanteaudio.currentTime= 0;
         redoblanteaudio.play();
     }
 
 let crashaudio = document.getElementById ("crash");
     function playaudio4() {
+        crashaudio.currentTime= 0;
         crashaudio.play();
      }
 
@@ -23,15 +27,18 @@ let crashaudio = document.getElementById ("crash");
 function cambio(){
 
     let btnSeepricing = document.getElementById("seepricing");
- 
-if (btnSeepricing.innerHTML === "see pricing"){
-    btnSeepricing.innerHTML = "It´s Not for you!";
-}
-else{
-    btnSeepricing.innerHTML = "see pricing";
-}
+    if (btnSeepricing.innerHTML === "see pricing"){
+        btnSeepricing.innerHTML = "It´s Not for you!";
+    }else{
+        btnSeepricing.innerHTML = "see pricing";
+    }
 }
 
-/*window.onload=function(){
-    alert('Nosotros somos ¡Los Timbales del Apocalipsis!');
-}*/
+let activePad = document.getElementById("ho");
+activePad.addEventListener("click", function(){
+    activePad.classList.add("press");
+
+    setTimeout(function(){
+    activePad.classList.remove("press")
+    }, 500);
+})
